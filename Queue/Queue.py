1402acca72bcf7976de75086b4acb840db83d469
Ronnen Nagal <nagalr@gmail.com>
2021-01-05 11:38:35 +0200

    Add Queue Impl

---
+++ Queue/Queue.py
@@ -0,0 +1,51 @@
+class Node:
+
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class Queue:
+
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.length = 0
+
+    def __repr__(self):
+        curr = self.first
+        result = []
+        while curr:
+            result.append(curr.value)
+            curr = curr.next
+        return str(result)
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.first:
+            self.last.next = new_node
+            self.last = new_node
+            self.length += 1
+        else:
+            self.first = new_node
+            self.last = new_node
+            self.length = 1
+
+    def pop(self):
+        if self.first:
+            temp = self.first
+            self.first = self.first.next
+            temp.next = None
+            self.length -= 1
+            return temp
+        else:
+            return None
+
+    def peek(self):
+        return self.first
+
+    def isEmpty(self):
+        return self.length == 0
