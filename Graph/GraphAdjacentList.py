400fffeea1800df4ba2848bd51d274737ae6c876
Ronnen Nagal <nagalr@gmail.com>
2021-01-06 15:16:30 +0200

    Add first Impl Graph with Adjacent List

---
+++ Graph/GraphAdjacentList.py
@@ -0,0 +1,48 @@
+class Graph:
+
+    def __init__(self):
+        self.NumberOfNodes = 0
+        self.adjacentList = {}
+
+    def __repr__(self):
+        return str(self.adjacentList)
+
+    def add_vertex(self, node):
+        self.adjacentList[node] = set()
+        self.NumberOfNodes += 1
+
+    def add_edge(self, node1, node2):
+        if node1 == node2:
+            return
+        if node1 not in self.adjacentList.keys():
+            self.add_vertex(node1)
+        if node2 not in self.adjacentList.keys():
+            self.add_vertex(node2)
+        curr_conn_list = self.adjacentList[node1]
+        curr_conn_list.add(node2)
+        self.adjacentList[node1] = curr_conn_list
+
+        curr_conn_list = self.adjacentList[node2]
+        curr_conn_list.add(node1)
+        self.adjacentList[node2] = curr_conn_list
+
+    def show_connections(self):
+        pass
+
+
+# Test
+g = Graph()
+g.add_vertex(2)
+g.add_edge(2,3)
+g.add_edge(2,4)
+g.add_edge(4,1)
+g.add_edge(4,1)
+g.add_edge(2,1)
+g.add_edge(2,1)
+g.add_edge(2,2)
+g.add_edge(2,2)
+g.add_edge(2,2)
+g.add_edge(2,2)
+g.add_edge(2,2)
+g.add_edge(2,2)
+print(g)
