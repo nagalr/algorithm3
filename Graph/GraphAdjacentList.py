34bd420654cbfb8a79d6f56d25dbffad5e0be934
Ronnen Nagal <nagalr@gmail.com>
2021-01-06 15:51:39 +0200

    Add test to Graph with Adjacent List

--- Graph/GraphAdjacentList.py
+++ Graph/GraphAdjacentList.py
@@ -21,3 +21,17 @@ class Graph:
 
         self.adjacentList[node1].add(node2)
         self.adjacentList[node2].add(node1)
+
+
+# Test
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 4)
+g.add_edge(3, 4)
+g.add_edge(4, 5)
+g.add_edge(5, 6)
+
+print(g)
