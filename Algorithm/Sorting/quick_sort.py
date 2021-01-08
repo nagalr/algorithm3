6ccdb0b8f9f800290d7a8991f4ee7ab7337ec8d4
Ronnen Nagal <nagalr@gmail.com>
2021-01-08 15:11:17 +0200

    Add a more efficient solution, less verbose quick sort Impl

--- Algorithm/Sorting/quick_sort.py
+++ Algorithm/Sorting/quick_sort.py
@@ -3,8 +3,6 @@ def quick_sort(l):
     Quick Sort Implementation.
     Time O(nlogn) Run.
     O(logn) space complexity.
-    :param l: List
-    :return: None
     """
     less = []
     equal = []
@@ -26,4 +24,22 @@ def quick_sort(l):
         return l
 
 
-print(quick_sort([12, 4, 5, 6, 7, 3, 1, 15, -1, -100, 38, 0, 1]))
+# A more efficient solution, less verbose
+def quick_sort2(l):
+    """
+    Quick Sort Implementation.
+    Time O(nlogn) Run.
+    O(logn) space complexity.
+    """
+    if len(l) <= 1:
+        return l
+
+    pivot  = l[len(l) // 2]
+    left   = [x for x in l if x < pivot]
+    middle = [x for x in l if x == pivot]
+    right  = [x for x in l if x > pivot]
+
+    return quick_sort2(left) + middle + quick_sort2(right)
+
+
+print(quick_sort2([12, 4, 5, 6, 7, 3, 1, 15, -1, -100, 38, 0, 1]))
